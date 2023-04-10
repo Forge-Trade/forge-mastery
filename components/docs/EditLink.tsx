a062a73de454ee1df78f5419dbfc7245821566e1
LPX <info@shapeshift.one>
2023-04-11 07:19:49 +0900

    merge

---
+++ components/docs/EditLink.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function EditLink() {
+    return (
+      <div>
+       Edit
+      </div>
+    )
+  }
+  
+  export default function() {
+    return <EditLink />
+  }
+  
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,30 +1,31 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import DocsFooter from './components/DocsFooter'
+import EditLink from './components/docs/EditLink'
 
 const config: DocsThemeConfig = {
   logo: <span>Forge DEX</span>,
-  feedback:{
-    content: null,
-  },
-  footer:{
-    component: DocsFooter
-  },
-  themeSwitch: {
-    component: <></>
-  },
-  editLink:{
-    component: <></>
-  },
-  sidebar: {
-    toggleButton: true
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Forge'
     }
   },
+  feedback:{
+    content: null,
+  },
+  footer:{
+    component: DocsFooter,
+  },
+  themeSwitch: {
+    component: <><div></div></>,
+  },
+  editLink:{
+    component: EditLink,
+  },
+  sidebar: {
+    toggleButton: true
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   primaryHue:{
     dark: 14,
     light: 14
