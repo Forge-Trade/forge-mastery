5d14777d9fb9b05427aa47291620329db341f5d1
LPX <hello@ethix.kr>
2023-05-24 23:45:07 +0900

    Update theme.config.tsx
    not kosher

--- theme.config.tsx
+++ theme.config.tsx
@@ -13,6 +13,19 @@ const config: DocsThemeConfig = {
   feedback:{
     content: null,
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content="ForgeMastery"/>
+      <meta property="og:description" content="Capital efficiency comes to the IBC ecosystem." />
+      <meta property="og:image" content="/forge-docs-og.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="ForgeMastery" />
+      <meta name="twitter:description" content="The way of the Forge." />
+      <meta name="twitter:image" content="/forge-docs-og.png" />
+    </>
+  ),
   footer:{
     component: DocsFooter,
   },
